8cf964780b571a3d4f78ddccfc1779fa43de0978
Linux_for_Randy <randy@pop-os.localdomain>
2022-05-20 16:13:02 +0700

    adding ruby on rails 5 module

---
+++ rubyonrails5/rails5_study_2/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20220324021549');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2022-03-24 02:17:22.064471','2022-03-24 02:17:22.064471');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Belajar Rails dengan Progate!','2022-03-24 02:22:16.124363','2022-03-24 02:22:16.124363');
+INSERT INTO posts VALUES(2,'Mencoba menampilkan post!','2022-03-24 02:22:36.338614','2022-03-24 02:22:36.338614');
+INSERT INTO posts VALUES(3,'Belajar code itu sangat menyenangkan!','2022-03-24 02:27:04.292506','2022-03-24 02:27:04.292506');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
